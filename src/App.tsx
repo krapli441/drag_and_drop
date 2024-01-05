f2d921ee5eb4f54064d1a9b76be6957a66898f83
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 16:39:36 +0900

    5x5 크기의 박스, 다시 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React, { CSSProperties, useState } from "react";
+import React, { CSSProperties } from "react";
 import "./App.css";
-
-type BoxSizing = "content-box" | "border-box";
 
 function App() {
   const containerStyle: CSSProperties = {
-    width: "50vw",
-    height: "50vh",
-    position: "relative",
+    width: "100vw",
+    height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -17,68 +14,27 @@ function App() {
     display: "grid",
     gridTemplateColumns: "repeat(5, 1fr)",
     gridTemplateRows: "repeat(5, 1fr)",
-    width: "100%",
-    height: "100%",
+    width: "25vw",
+    height: "25vw",
+    border: "2px solid black",
   };
 
   const boxStyle: CSSProperties = {
     border: "1px solid black",
-    boxSizing: "border-box" as BoxSizing,
   };
 
   const inventoryBoxes = [];
 
   for (let i = 0; i < 25; i++) {
-    const handleDrop = (e: any) => {
-      e.preventDefault();
-      const itemData = e.dataTransfer.getData("item");
-      e.target.appendChild(document.getElementById(itemData));
-    };
-
-    const handleDragOver = (e: any) => {
-      e.preventDefault();
-    };
-
     inventoryBoxes.push(
-      <div
-        key={i}
-        style={{ ...boxStyle, cursor: "pointer" }}
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-      >
-        {i + 1}
-      </div>
+      <div key={i} style={{ ...boxStyle, cursor: "default" }}></div>
     );
   }
-
-  const items = [
-    { id: "item1", content: "Item 1" },
-    { id: "item2", content: "Item 2" },
-    // Add more items here
-  ];
-
-  const handleDragStart = (e: any, item: any) => {
-    e.dataTransfer.setData("item", item.id);
-  };
-
-  const itemComponents = items.map((item) => (
-    <div
-      key={item.id}
-      style={{ ...boxStyle, cursor: "grab" }}
-      draggable={true}
-      onDragStart={(e) => handleDragStart(e, item)}
-    >
-      {item.content}
-    </div>
-  ));
 
   return (
     <div className="App">
       <div style={containerStyle}>
         <div style={inventoryStyle}>{inventoryBoxes}</div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          {itemComponents}
-        </div>
       </div>
     </div>
   );
