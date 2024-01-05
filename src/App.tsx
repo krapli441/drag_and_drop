43e47b6e864f8f26ff41dd6544138ffc95af7a9f
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 17:03:11 +0900

    1x1, 2x2, 3x3 추가

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
     display: "grid",
     gridTemplateColumns: "repeat(5, 1fr)",
     gridTemplateRows: "repeat(5, 1fr)",
-    width: "20%",
-    height: "50%",
+    width: "300px",
+    height: "300px",
     border: "2px solid black",
   };
 
@@ -31,12 +31,45 @@ function App() {
     );
   }
 
+  // 1x1, 2x2, 3x3 크기의 아이템 추가
+  const items = [
+    { size: "1x1", text: "1x1", width: "60px", height: "60px" },
+    { size: "2x2", text: "2x2", width: "120px", height: "120px" },
+    { size: "3x3", text: "3x3", width: "180px", height: "180px" },
+  ];
+
+  const itemComponents = items.map((item, index) => (
+    <div
+      key={index}
+      draggable={true}
+      style={{
+        width: item.width,
+        height: item.height,
+        cursor: "grab",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid black",
+      }}
+    >
+      {item.text}
+    </div>
+  ));
+
   return (
     <div className="App">
       <div style={inventoryContainerStyle}>
         <div style={inventoryBoxStyle}>{inventoryBoxes}</div>
       </div>
-      <div style={{ height: "30vh" }}>{/* 물건을 정렬할 공간 */}</div>
+      <div
+        style={{
+          height: "30vh",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        {itemComponents}
+      </div>
     </div>
   );
 }
