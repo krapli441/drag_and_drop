812e8ee0bfab89f39f73df952e549056eec94d07
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 16:53:22 +0900

    다시, 두 개의 구역으로 우선 나눔

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,12 @@ import React, { CSSProperties } from "react";
 import "./App.css";
 
 function App() {
-  const containerStyle: CSSProperties = {
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
-  const inventoryContainerstyle: CSSProperties = {
-    height: "70vh",
-  };
-
-  const inventoryStyle: CSSProperties = {
-    display: "grid",
-    gridTemplateColumns: "repeat(5, 1fr)",
-    gridTemplateRows: "repeat(5, 1fr)",
-    width: "25vw",
-    height: "25vw",
-    border: "2px solid black",
-  };
-
-  const boxStyle: CSSProperties = {
-    border: "1px solid black",
-  };
-
-  const inventoryBoxes = [];
-
-  for (let i = 0; i < 25; i++) {
-    inventoryBoxes.push(
-      <div key={i} style={{ ...boxStyle, cursor: "default" }}></div>
-    );
-  }
-
   return (
     <div className="App">
-      <div style={containerStyle}>
-        <div style={inventoryContainerstyle}>
-          <div style={inventoryStyle}>{inventoryBoxes}</div>
-        </div>
+      <div style={{ height: "70vh", borderBottom: "2px solid black" }}>
+        {/* 5x5 인벤토리가 들어갈 영역 */}
       </div>
+      <div style={{ height: "30vh" }}>{/* 물건을 정렬할 공간 */}</div>
     </div>
   );
 }
