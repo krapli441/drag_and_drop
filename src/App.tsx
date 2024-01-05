551f7ffeeb137dbc5d5bae1acb2034c039b9c63f
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 18:51:10 +0900

    grid 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./App.css";
 
+// 초기 인벤토리 배열
+const inventory = [
+  [null, null, null, null, null],
+  [null, null, null, null, null],
+  [null, null, null, null, null],
+  [null, null, null, null, null],
+  [null, null, null, null, null],
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="inventory">
+        {inventory.map((row, rowIndex) => (
+          <div className="inventory-row" key={rowIndex}>
+            {row.map((item, columnIndex) => (
+              <div className="inventory-cell" key={columnIndex}>
+                {item !== null ? (
+                  <div className="inventory-item">
+                    {/* 아이템 표시: 여기에 아이템 정보를 표시 */}
+                    {/* {item.name} */}
+                  </div>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
