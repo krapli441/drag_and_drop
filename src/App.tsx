0fa89fd6cf2ebd2d09a78db9428aab06b95c668f
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 18:56:42 +0900

    메세지 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 // 초기 인벤토리 배열
@@ -11,24 +11,38 @@ const inventory = [
 ];
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: -1, y: -1 });
+
+  const handleMouseEnter = (rowIndex: any, columnIndex: any) => {
+    setMousePosition({ x: rowIndex, y: columnIndex });
+  };
+
   return (
     <div className="App">
       <div className="inventory">
         {inventory.map((row, rowIndex) => (
           <div className="inventory-row" key={rowIndex}>
             {row.map((item, columnIndex) => (
-              <div className="inventory-cell" key={columnIndex}>
+              <div
+                className="inventory-cell"
+                key={columnIndex}
+                onMouseEnter={() => handleMouseEnter(rowIndex, columnIndex)}
+              >
                 {item !== null ? (
-                  <div className="inventory-item">
-                    {/* 아이템 표시: 여기에 아이템 정보를 표시 */}
-                    {/* {item.name} */}
-                  </div>
+                  <div className="inventory-item">{/* 아이템 표시 */}</div>
                 ) : null}
               </div>
             ))}
           </div>
         ))}
       </div>
+      {mousePosition.x !== -1 && mousePosition.y !== -1 && (
+        <div>
+          {`${mousePosition.x + 1}번째 줄의 ${
+            mousePosition.y + 1
+          }번째 칸에 마우스를 올렸습니다`}
+        </div>
+      )}
     </div>
   );
 }
