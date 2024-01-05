d100a5753fe8502e35618173ecd5c735c8ca8efb
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-05 16:55:49 +0900

    윗쪽 영역에 5x5 박스 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@ import React, { CSSProperties } from "react";
 import "./App.css";
 
 function App() {
+  const inventoryContainerStyle: CSSProperties = {
+    height: "70vh",
+    borderBottom: "2px solid black",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const inventoryBoxStyle: CSSProperties = {
+    display: "grid",
+    gridTemplateColumns: "repeat(5, 1fr)",
+    gridTemplateRows: "repeat(5, 1fr)",
+    width: "20%",
+    height: "50%",
+    border: "2px solid black",
+  };
+
+  const boxStyle: CSSProperties = {
+    border: "1px solid black",
+  };
+
+  const inventoryBoxes = [];
+
+  for (let i = 0; i < 25; i++) {
+    inventoryBoxes.push(
+      <div key={i} style={{ ...boxStyle, cursor: "default" }}></div>
+    );
+  }
+
   return (
     <div className="App">
-      <div style={{ height: "70vh", borderBottom: "2px solid black" }}>
-        {/* 5x5 인벤토리가 들어갈 영역 */}
+      <div style={inventoryContainerStyle}>
+        <div style={inventoryBoxStyle}>{inventoryBoxes}</div>
       </div>
       <div style={{ height: "30vh" }}>{/* 물건을 정렬할 공간 */}</div>
     </div>
