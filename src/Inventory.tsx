4c371093ee01400c5f656cf8f969c90e0c2888a6
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-08 13:52:27 +0900

    샘플 아이템 -items 추가

--- src/Inventory.tsx
+++ src/Inventory.tsx
@@ -1,10 +1,49 @@
 import React, { useState } from "react";
 import "./Inventory.css";
 
+const _items = [
+  {
+    name: "AK-47",
+    image:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158749270831214/ak47_2.png)",
+    image2:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158749040136223/ak47.png)",
+    size_width: 2,
+    size_height: 4,
+  },
+  {
+    name: "Mag",
+    image:
+      "url(https://media.discordapp.net/attachments/656601175763845180/930158750952730705/mag.png)",
+    image2:
+      "url(https://media.discordapp.net/attachments/656601175763845180/930158751183425586/mag_2.png)",
+    size_width: 1,
+    size_height: 2,
+  },
+  {
+    name: "AMMO",
+    image:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158749509910538/ammo.png)",
+    image2:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158749769941042/ammo_2.png)",
+    size_width: 1,
+    size_height: 1,
+  },
+  {
+    name: "Backpack",
+    image:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158750101274684/backpack.png)",
+    image2:
+      "url(https://cdn.discordapp.com/attachments/656601175763845180/930158750629785650/backpack_2.png)",
+    size_width: 5,
+    size_height: 7,
+  },
+];
+
 const Inventory = () => {
   const [grid, setGrid] = useState({
-    width_block: 5,
-    height_block: 5,
+    width_block: 10,
+    height_block: 10,
     size: 50,
   });
 
