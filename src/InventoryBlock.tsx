d294c3376dfc71a32e36e31fb5fdc45f5b64324d
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-09 13:35:57 +0900

    InvetoryBlock 계속 수정, map 메서드 들어가야 함

--- src/InventoryBlock.tsx
+++ src/InventoryBlock.tsx
@@ -18,7 +18,27 @@ const InventoryBlock: React.FC<InventoryBlockProps> = ({
 
   return (
     <div>
-
+      {itemBlocks.map((item, index) => (
+        <div
+          key={index}
+          className="block"
+          style={{ width: size, height: size }}
+        >
+          {item && (
+            <div
+              className="item"
+              style={{
+                // 예시: 아이템의 배경 이미지, 크기 등을 설정
+                backgroundImage: item.image,
+                width: item.size_width * size,
+                height: item.size_height * size,
+              }}
+            >
+              {/* 아이템의 추가적인 내용 렌더링 */}
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
