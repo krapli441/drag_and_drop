9f125299a61cab5e1e56d86c1a148f016909786d
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-09 17:18:46 +0900

    그리드 정렬.

--- src/scene/InventoryScene.ts
+++ src/scene/InventoryScene.ts
@@ -21,6 +21,9 @@ export default class InventoryScene extends Phaser.Scene {
 
     this.input.setDraggable(this.item);
     this.input.keyboard = this.input.keyboard;
+    this.item.on("pointerup", (pointer: Phaser.Input.Pointer) => {
+      this.alignItemToGrid(this.item);
+    });
 
     this.input.on(
       "drag",
@@ -53,7 +56,6 @@ export default class InventoryScene extends Phaser.Scene {
     });
 
     // 키 입력 이벤트 리스너
-
     if (this.input && this.input.keyboard) {
       this.input.keyboard.on("keydown-R", () => {
         // 아이템이 클릭된 상태에서 'R' 키가 눌렸을 때만 회전
@@ -74,5 +76,16 @@ export default class InventoryScene extends Phaser.Scene {
       graphics.lineTo(500, i);
     }
     graphics.strokePath();
+  }
+
+  alignItemToGrid(item: Phaser.GameObjects.Sprite) {
+    const gridSize = 50; // 그리드 칸의 크기
+    // 아이템의 드래그 종료 위치를 그리드 크기로 나누어 가장 가까운 칸 위치 계산
+    const gridX = Math.round(item.x / gridSize) * gridSize;
+    const gridY = Math.round(item.y / gridSize) * gridSize;
+
+    // 아이템을 그리드 칸의 중앙으로 이동
+    item.x = gridX;
+    item.y = gridY;
   }
 }
