4ca47ba25ffd43fc4aabe785e98f344e55c96af0
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-09 13:00:34 +0900

    types.ts, Inventory.tsx 생성

---
+++ src/Inventory.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { Item, Grid } from "./Types";
+
+const _items: Item[] = [];
---
+++ src/Types.ts
@@ -0,0 +1,13 @@
+export interface Item {
+  name: string;
+  image: string;
+  image2: string;
+  size_width: number;
+  size_height: number;
+}
+
+export interface Grid {
+  width_block: number;
+  height_block: number;
+  size: number;
+}
