97e9aff67932e24313f469f8beb9d259b0eda58a
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-22 16:04:58 +0900

    간단한 검사 메서드 넣었지만 아이템 넣지 못하고 있음

--- src/scenes/InventoryClass/Inventory.ts
+++ src/scenes/InventoryClass/Inventory.ts
@@ -5,17 +5,32 @@ import { ChestRigProperties } from "./InventoryInterface";
 export default class Inventory {
   private grids: Grid[];
   private capacity: number;
+  private numColumns: number; // 그리드의 열 수 추가
 
   constructor(properties: ChestRigProperties) {
     this.grids = properties.grids.map(
       (grid) => new Grid(grid.width, grid.height, grid.x, grid.y)
     );
     this.capacity = properties.capacity;
+    this.numColumns = this.calculateNumColumns(properties.grids); // 그리드의 열 수 계산
+  }
+
+  // 그리드 열 수를 계산하는 메서드
+  private calculateNumColumns(grids: Grid[]): number {
+    let maxRightEdge = 0;
+
+    grids.forEach((grid) => {
+      const rightEdge = grid.x + grid.width;
+      if (rightEdge > maxRightEdge) {
+        maxRightEdge = rightEdge;
+      }
+    });
+
+    return maxRightEdge;
   }
 
   // 아이템 추가 메서드
   addItem(item: Item, position: { x: number; y: number }): boolean {
-    // 아이템이 그리드에 들어갈 수 있는지 검사
     if (!this.isItemFit(item, position)) {
       return false;
     }
@@ -23,16 +38,9 @@ export default class Inventory {
     // 아이템을 그리드에 추가하는 로직
     for (let i = 0; i < item.height; i++) {
       for (let j = 0; j < item.width; j++) {
-        // 그리드의 인덱스를 계산
-        const gridIndex =
-          position.y * this.grids.length +
-          position.x +
-          i * this.grids.length +
-          j;
-        if (
-          gridIndex < this.grids.length &&
-          !this.grids[gridIndex].addItem(item)
-        ) {
+        // 그리드의 인덱스를 올바르게 계산
+        const gridIndex = (position.y + i) * this.numColumns + (position.x + j);
+        if (!this.grids[gridIndex].addItem(item)) {
           return false;
         }
       }
@@ -43,15 +51,9 @@ export default class Inventory {
 
   // 아이템이 해당 위치에 맞게 들어갈 수 있는지 검사하는 메서드
   private isItemFit(item: Item, position: { x: number; y: number }): boolean {
-    // 아이템이 인벤토리 내 위치에 맞게 들어갈 수 있는지 검사
     for (let i = 0; i < item.height; i++) {
       for (let j = 0; j < item.width; j++) {
-        // 그리드의 인덱스를 계산
-        const gridIndex =
-          position.y * this.grids.length +
-          position.x +
-          i * this.grids.length +
-          j;
+        const gridIndex = (position.y + i) * this.numColumns + (position.x + j);
         if (gridIndex >= this.grids.length || this.grids[gridIndex].item) {
           return false;
         }
