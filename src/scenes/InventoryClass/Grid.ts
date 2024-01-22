e486778dd81d440639f0eb1dc5e0cb1d2d6c5e49
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-22 12:41:16 +0900

    Grid.ts 생성자 및 클래스 변경

--- src/scenes/InventoryClass/Grid.ts
+++ src/scenes/InventoryClass/Grid.ts
@@ -3,7 +3,7 @@ import Item from "./Item";
 export default class Grid {
   width: number;
   height: number;
-  item: Item | null;
+  items: (Item | null)[];
   x: number;
   y: number;
 
@@ -12,21 +12,17 @@ export default class Grid {
     this.height = height;
     this.x = x;
     this.y = y;
-    this.item = Array(width * height).fill(null);
+    this.items = Array(width * height).fill(null);
   }
 
   // 그리드에 아이템을 추가하는 메서드
-  addItem(item: Item): boolean {
-    // 이 그리드에 아이템이 들어갈 수 있는지 검사
-    if (
-      this.item !== null ||
-      item.width > this.width ||
-      item.height > this.height
-    ) {
-      return false; // 아이템이 들어갈 수 없다면 false 반환
+  // 특정 위치에 아이템을 추가하는 메서드
+  addItem(item: Item, position: number): boolean {
+    // position은 아이템을 추가할 배열 인덱스
+    if (this.items[position] === null) {
+      this.items[position] = item; // 아이템을 해당 위치에 추가
+      return true; // 성공적으로 추가되면 true 반환
     }
-
-    this.item = item; // 아이템을 그리드에 추가
-    return true; // 성공적으로 추가되면 true 반환
+    return false; // 해당 위치에 이미 아이템이 있으면 false 반환
   }
 }
