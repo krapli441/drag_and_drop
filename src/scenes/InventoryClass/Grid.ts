bd19329f38a9aacc0b5413ee5b9854832e16960d
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-22 12:42:29 +0900

    오류 다수 발생, 수정 필요

--- src/scenes/InventoryClass/Grid.ts
+++ src/scenes/InventoryClass/Grid.ts
@@ -3,7 +3,7 @@ import Item from "./Item";
 export default class Grid {
   width: number;
   height: number;
-  items: (Item | null)[];
+  item: (Item | null)[];
   x: number;
   y: number;
 
@@ -12,15 +12,15 @@ export default class Grid {
     this.height = height;
     this.x = x;
     this.y = y;
-    this.items = Array(width * height).fill(null);
+    this.item = Array(width * height).fill(null);
   }
 
   // 그리드에 아이템을 추가하는 메서드
   // 특정 위치에 아이템을 추가하는 메서드
   addItem(item: Item, position: number): boolean {
     // position은 아이템을 추가할 배열 인덱스
-    if (this.items[position] === null) {
-      this.items[position] = item; // 아이템을 해당 위치에 추가
+    if (this.item[position] === null) {
+      this.item[position] = item; // 아이템을 해당 위치에 추가
       return true; // 성공적으로 추가되면 true 반환
     }
     return false; // 해당 위치에 이미 아이템이 있으면 false 반환
