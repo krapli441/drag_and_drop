eb1527bd17cb6b2880eaba4a631a0796a26eef7d
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-23 17:08:56 +0900

    아이템 정보 불러오지만 두 번 불러오고 있음

--- src/scenes/Inventory.ts
+++ src/scenes/Inventory.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import { loadChestRigData, loadBarterItemsData } from "../api/api";
+import { loadRandomData } from "../api/api";
 import { CHEST_RIG_QUERY, BARTER_ITEM_QUERY } from "../api/item_queries";
 
 export default class Inventory extends Phaser.Scene {
@@ -9,5 +9,15 @@ export default class Inventory extends Phaser.Scene {
 
   preload() {}
 
-  async create() {}
+  async create() {
+    try {
+      const chestRigData = await loadRandomData(CHEST_RIG_QUERY);
+      const barterItemsData = await loadRandomData(BARTER_ITEM_QUERY);
+      // 여기에서 받은 데이터를 처리합니다.
+      console.log("체스트 리그 데이터 : ", chestRigData);
+      console.log("교환 아이템 데이터 : ", barterItemsData);
+    } catch (error) {
+      console.error("데이터 로딩 중 오류가 발생했습니다:", error);
+    }
+  }
 }
