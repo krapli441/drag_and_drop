b50e691a212fd09b2c7c17ca242eefa3e728e66a
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-24 14:57:03 +0900

    내부 그리드 레이아웃 그리는 로직 추가

--- src/scenes/Inventory.ts
+++ src/scenes/Inventory.ts
@@ -24,6 +24,10 @@ export default class Inventory extends Phaser.Scene {
 
       console.log("무작위 체스트 리그 데이터 : ", randomChestRig);
       console.log("무작위 교환 아이템 데이터 : ", randomBarterItems);
+
+      if (randomChestRig.hasGrid && randomChestRig.properties.grids) {
+        this.drawGrid(randomChestRig.properties.grids);
+      }
     } catch (error) {
       console.error("데이터 로딩 중 오류가 발생했습니다:", error);
     }
@@ -37,5 +41,29 @@ export default class Inventory extends Phaser.Scene {
   selectRandomBarterItems(items: BarterItem[], count: number) {
     const shuffled = items.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
+  }
+
+  drawGrid(grids: any[]) {
+    const gridGraphics = this.add.graphics();
+    gridGraphics.lineStyle(2, 0xffffff, 1); // 테두리 스타일 설정
+
+    const gridSize = 50; // 각 그리드 칸의 크기
+    let currentX = 0; // 현재 X 좌표
+    let currentY = 0; // 현재 Y 좌표
+
+    grids.forEach((grid) => {
+      const gridWidth = grid.width * gridSize;
+      const gridHeight = grid.height * gridSize;
+
+      // 그리드를 그립니다
+      gridGraphics.strokeRect(currentX, currentY, gridWidth, gridHeight);
+
+      // 다음 그리드의 위치를 계산합니다
+      currentX += gridWidth;
+      if (currentX >= Number(this.sys.game.config.width)) {
+        currentX = 0;
+        currentY += gridHeight;
+      }
+    });
   }
 }
