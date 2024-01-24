b3adc26f072c7a7cd949ea5cfa794f3361b9d87e
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-24 11:05:34 +0900

    필요 없어진 console.log 삭제, 무작위 체스트 리그 / 아이템 선정

--- src/scenes/Inventory.ts
+++ src/scenes/Inventory.ts
@@ -5,22 +5,35 @@ import { CHEST_RIG_QUERY, BARTER_ITEM_QUERY } from "../api/item_queries";
 export default class Inventory extends Phaser.Scene {
   constructor() {
     super("Inventory");
-    console.log("Inventory 씬 생성자 호출됨");
   }
 
-  preload() {
-    console.log("Inventory 씬의 preload 메소드 호출됨");
-  }
+  preload() {}
 
   async create() {
     console.log("Inventory 씬의 create 메소드 호출됨");
     try {
       const chestRigData = await loadRandomData(CHEST_RIG_QUERY);
       const barterItemsData = await loadRandomData(BARTER_ITEM_QUERY);
-      console.log("체스트 리그 데이터 : ", chestRigData);
-      console.log("교환 아이템 데이터 : ", barterItemsData);
+      const randomChestRig = this.selectRandomChestRig(chestRigData.data.items);
+      const randomBarterItems = this.selectRandomBarterItems(
+        barterItemsData.data.items,
+        5
+      );
+
+      console.log("무작위 체스트 리그 데이터 : ", randomChestRig);
+      console.log("무작위 교환 아이템 데이터 : ", randomBarterItems);
     } catch (error) {
       console.error("데이터 로딩 중 오류가 발생했습니다:", error);
     }
+  }
+
+  selectRandomChestRig(items) {
+    const randomIndex = Math.floor(Math.random() * items.length);
+    return items[randomIndex];
+  }
+
+  selectRandomBarterItems(items, count) {
+    const shuffled = items.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
   }
 }
