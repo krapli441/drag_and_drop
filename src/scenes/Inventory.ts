13ccb3a525f2e5ecd31ee5354e3b398119106e6c
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-24 15:28:24 +0900

    간격 추가

--- src/scenes/Inventory.ts
+++ src/scenes/Inventory.ts
@@ -46,13 +46,14 @@ export default class Inventory extends Phaser.Scene {
   drawGrid(grids: any[]) {
     const gridGraphics = this.add.graphics();
     gridGraphics.lineStyle(2, 0xffffff, 1); // 테두리 스타일 설정
-  
+
     const gridSize = 50; // 각 그리드 칸의 크기
+    const gridSpacing = 10; // 그리드 사이의 간격
     let currentX = 0; // 현재 X 좌표
     let currentY = 0; // 현재 Y 좌표
     let maxYInRow = 0; // 현재 행에서 가장 높은 그리드의 높이
-  
-    grids.forEach(grid => {
+
+    grids.forEach((grid) => {
       for (let i = 0; i < grid.width; i++) {
         for (let j = 0; j < grid.height; j++) {
           const x = currentX + i * gridSize;
@@ -60,19 +61,17 @@ export default class Inventory extends Phaser.Scene {
           gridGraphics.strokeRect(x, y, gridSize, gridSize);
         }
       }
-  
-      // 다음 그리드 위치 업데이트
-      currentX += grid.width * gridSize;
+
+      // 다음 그리드 위치 업데이트 (간격 추가)
+      currentX += grid.width * gridSize + gridSpacing;
       maxYInRow = Math.max(maxYInRow, grid.height * gridSize);
-  
-      // 줄바꿈 처리
+
+      // 줄바꿈 처리 (새 행 시작 시 간격 추가)
       if (currentX + gridSize > Number(this.sys.game.config.width)) {
         currentX = 0;
-        currentY += maxYInRow;
+        currentY += maxYInRow + gridSpacing;
         maxYInRow = 0;
       }
     });
   }
-  
-  
 }
